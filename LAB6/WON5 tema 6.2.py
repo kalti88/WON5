e3ac6491be07c4446d04f84bfcc22260429c198d
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2022-12-11 15:01:41 +0200

    Tema 5

--- LAB6/WON5 tema 6.2.py
+++ LAB6/WON5 tema 6.2.py
@@ -6,7 +6,7 @@ catalog = {
     'Georgescu Gelu': [4, 2],
     'Radulescu Ioana': [5, 9, 6, 4, 10]
 }
-antet = ['Nume', 'Prenume', 'Media', '_']
+antet = ['Nume1', 'Prenume1', 'Media1', '_']
 print(f'{antet[0]:<25}{antet[1]:<25}{antet[2]:5}\n{antet[3]*55}')
 for e in catalog:
     print(f'{e.split()[0]:<25}{e.split()[1]:<25}{statistics.mean(catalog[e]):5.2f}')
