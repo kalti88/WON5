271b7f0fd6704206fbac69c3ea0c9591d3065ac5
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2022-12-18 23:12:54 +0200

    Tema 11_5b

---
+++ LAB11/WON5_tema_11_5b.py
@@ -0,0 +1,22 @@
+import os
+
+
+def total_size(address):
+    total = 0
+
+    def local_total_size(address):
+        nonlocal total
+        for item in os.listdir(address):
+            if os.path.isfile(f'{address}/{item}'):
+                total += os.path.getsize(f'{address}/{item}')
+            elif os.path.isdir(f'{address}/{item}'):
+                ad = f'{address}/{item}'
+                local_total_size(ad)
+    local_total_size(address)
+    return total
+
+
+address = 'C:/Users/Zsolt/PycharmProjects/PITHON_WON5/'
+total = total_size(address)
+print(address)
+print('Total file size {} Megabytes'.format(total/(1024**2)))
