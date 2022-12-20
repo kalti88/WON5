8f5fa2a7c5d983d74f8c7669e00c39d468c468db
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2022-12-20 19:19:22 +0200

    Last changes

--- LAB11/WON5_tema_11_3.py
+++ LAB11/WON5_tema_11_3.py
@@ -7,5 +7,5 @@ for root, dirs, files in os.walk(address):
         ext = f.split('.')[-1].lower()
         if ext == 'py':
             count_py += 1
-            print(f'{roots}/{f}')
+            print(f'{root}/{f}')
 print('Nr. Python files {}'.format(count_py))
--- LAB11/WON5_tema_11_4.py
+++ LAB11/WON5_tema_11_4.py
@@ -4,6 +4,6 @@ total = 0
 address = 'C:/Users/Zsolt/PycharmProjects/PITHON_WON5'
 for root, dirs, files in os.walk(address):
     for f in files:
-        total += os.path.getsize(f'{roots}/{f}')
+        total += os.path.getsize(f'{root}/{f}')
 print(address)
 print('Total file size {} Megabytes'.format(f'{total/(1024**2):.2f}'))
