6a2f94007f31026f49bc9ba796c68414dc26a6fd
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2022-12-21 22:48:19 +0200

    Tema 12_1

---
+++ LAB12/WON5_tema_12_1.py
@@ -0,0 +1,19 @@
+class Point:
+
+    def __init__(self, coordonate_x: float, coordonate_y: float):
+        """Constructor of points"""
+        self.x = coordonate_x
+        self.y = coordonate_y
+
+    def get_distance(self):
+        distance = (self.x ** 2 + self.y ** 2) ** 0.5
+        return f'{distance:,.2f}'
+
+
+x = float(input('Inrodu valoarea coordonatei x!'))
+y = float(input('Inrodu valoarea coordonatei y!'))
+
+p1 = Point(x, y)
+
+print('Distanta de la punctul de la origine {}'.format(p1.get_distance()))
+
