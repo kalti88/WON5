dbc31fe109fdcd9baad5579e99cf194d015f207b
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2022-12-31 18:00:53 +0200

    Tema 12_5

--- LAB12/WON5_tema_12_5.py
+++ LAB12/WON5_tema_12_5.py
@@ -4,9 +4,9 @@ P1 = geometry.Point(2, 9)
 P2 = geometry.Point(-4, 11)
 P3 = geometry.Point(7, 3)
 P4 = geometry.Point(-4, 6)
-C1 = geometry.Circle(3, 5, 6)
+C1 = geometry.Circle(3, 4, 4)
 
-li = [P1, P2, P3, P4]
+li = [P1, P2, P3, P4, C1]
 li_sort = sorted(li, key=lambda x: x.get_distance())
 for i in li:
     print(f'{i.get_distance():,.4f}')
