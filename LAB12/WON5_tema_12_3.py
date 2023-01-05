84f35f47a8231a859988ef9fee5431eab7581d70
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-01-05 19:16:03 +0200

    Tema 12_3

--- LAB12/WON5_tema_12_3.py
+++ LAB12/WON5_tema_12_3.py
@@ -1,12 +1,12 @@
-import geometry
+from geometry import Point
 
 x1 = float(input('Inrodu valoarea coordonatei x al puntului P1!'))
 y1 = float(input('Inrodu valoarea coordonatei y al puntului P1!'))
 x2 = float(input('Inrodu valoarea coordonatei x al puntului P2!'))
 y2 = float(input('Inrodu valoarea coordonatei y al puntului P2!'))
 
-P1 = geometry.Point(x1, y1)
-P2 = geometry.Point(x2, y2)
+P1 = Point(x1, y1)
+P2 = Point(x2, y2)
 
 if P1.get_distance() == P2.get_distance():
     print('P1 si P2 sunt la aceeasi distanta fata de centru.')
