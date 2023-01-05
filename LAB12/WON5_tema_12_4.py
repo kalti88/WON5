6aa6fd180ac83e750084438332c0df6e5b869dcd
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-01-05 19:19:45 +0200

    Tema 12_4

--- LAB12/WON5_tema_12_4.py
+++ LAB12/WON5_tema_12_4.py
@@ -1,11 +1,11 @@
-import geometry
+from geometry import Circle
 
 x = float(input('Inrodu valoarea coordonatei x al centrul cercului!'))
 y = float(input('Inrodu valoarea coordonatei y al centrul cercului!'))
 raza = float(input('Inrodu valoarea razei cercului!'))
 
-C = geometry.Circle(x, y, raza)
+C = Circle(x, y, raza)
 
-print('Aria cercului este {}'.format(f'{C.get_area():,.2f}'))
+print(f'Aria cercului este {C.get_area():,.2f}')
 print('Distanta cercului de la centru este {}'.format(C.get_distance()))
 
