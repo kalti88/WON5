691c3739c8b08b3eb9e04cba30adbea862859480
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-01-07 22:44:12 +0200

    Tema 13_1

--- LAB13/geometry.py
+++ LAB13/geometry.py
@@ -8,7 +8,7 @@ class Point:
         :param y: y-axis coordinate
         :type y: float
 
-        :returns: object
+        :return: object
         """
         self.x = x
         self.y = y
@@ -18,8 +18,40 @@ class Rectangle:
 
     def __init__(self, p1, p2):
         """
-        Define a resctangle
-        :param p1:
-        :param p2:
-        """
+        Define a rectangle
+        :param p1: First construction point
+        :type p1: Point
+        :param p2: Second construction point
+        :type p2: Point
 
+        :return: object
+        """
+        self.p1 = p1
+        self.p2 = p2
+
+    def length_of_sides(self):
+        """
+        Calculate the lengths of sides
+        :returns:
+            lx - the length of side on x axes
+            ly - the length of side on y axes
+        """
+        lx = abs(self.p1.x - self.p2.x)
+        ly = abs(self.p1.y - self.p2.y)
+        return lx, ly
+
+    def rectangle_area(self):
+        """
+        Calculate the area of a rectangle.
+        :return: The area
+        """
+        area = abs((self.p1.x - self.p2.x) * (self.p1.y - self.p2.y))
+        return area
+
+    def rectangle_perimeter(self):
+        """
+        Calculate the perimeter of a rectangle.
+        :return: The perimeter
+        """
+        prm = abs((self.p1.x - self.p2.x) + (self.p1.y - self.p2.y)) * 2
+        return prm
