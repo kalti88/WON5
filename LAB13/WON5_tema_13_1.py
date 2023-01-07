35398b5e1a59a46716f50922c4323388bf24688e
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-01-07 22:53:35 +0200

    Tema 13_1

--- LAB13/WON5_tema_13_1.py
+++ LAB13/WON5_tema_13_1.py
@@ -1,8 +1,18 @@
-from geometry import Point
+from geometry import Point, Rectangle
 
-x = float(input('Inrodu valoarea coordonatei x!'))
-y = float(input('Inrodu valoarea coordonatei y!'))
+x1 = float(input('Inrodu valoarea coordonatei x pentru punctul A!'))
+y1 = float(input('Inrodu valoarea coordonatei y pentru punctul A!'))
+x2 = float(input('Inrodu valoarea coordonatei x pentru punctul B!'))
+y2 = float(input('Inrodu valoarea coordonatei y pentru punctul B!'))
 
-p1 = Point(x, y)
+p1 = Point(x1, y1)
+p2 = Point(x2, y2)
 
-print(f'Distanta de la punct la origine {p1.get_distance():,.2f}')
+R = Rectangle(p1, p2)
+l1, l2 = R.length_of_sides()
+prm = R.rectangle_perimeter()
+area = R.rectangle_area()
+
+print(f'Lungimea laturilor dreptunghiului format din puntele A si B sunt {l1:,.2f} si {l2:,.2f}')
+print(f'Perimetrul dreptunghiului este {prm:,.2f}')
+print(f'Aria dreptunghiului este {area:,.2f}')
