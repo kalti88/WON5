34a7ca4be4dc4027972f248d69999078f5dd0c09
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-01-08 13:40:27 +0200

    Tema 13 last changes

--- LAB13/WON5_tema_13_4.py
+++ LAB13/WON5_tema_13_4.py
@@ -3,7 +3,11 @@ from geometry import Point, Circle, Rectangle
 x1 = float(input('Inrodu valoarea coordonatei x pentru punctul A!'))
 y1 = float(input('Inrodu valoarea coordonatei y pentru punctul A!'))
 x2 = float(input('Inrodu valoarea coordonatei x pentru punctul B!'))
+while x2 == x1:
+    x2 = float(input(f'Valoarea coordonatei x pentru punctul B trebuie sa fie diferita de {x1}. Alege alt numar!'))
 y2 = float(input('Inrodu valoarea coordonatei y pentru punctul B!'))
+while y2 == y1:
+    y2 = float(input(f'Valoarea coordonatei x pentru punctul B trebuie sa fie diferita de {y1}. Alege alt numar!'))
 
 print('Cercul pentru a verifica daca e sau nu in interiorul dreptunghiului:')
 x = float(input('Inrodu valoarea coordonatei x!'))
--- LAB13/WON5_tema_13_5.py
+++ LAB13/WON5_tema_13_5.py
@@ -2,12 +2,20 @@ from geometry import Point, Rectangle
 
 x11 = float(input('Inrodu valoarea coordonatei x pentru punctul A1!'))
 y11 = float(input('Inrodu valoarea coordonatei y pentru punctul A1!'))
-x12 = float(input('Inrodu valoarea coordonatei x pentru punctul B1!'))
-y12 = float(input('Inrodu valoarea coordonatei y pentru punctul B1!'))
+x12 = float(input('Inrodu valoarea coordonatei x pentru punctul B!'))
+while x12 == x11:
+    x12 = float(input(f'Valoarea coordonatei x pentru punctul B trebuie sa fie diferita de {x11}. Alege alt numar!'))
+y12 = float(input('Inrodu valoarea coordonatei y pentru punctul B!'))
+while y12 == y11:
+    y12 = float(input(f'Valoarea coordonatei x pentru punctul B trebuie sa fie diferita de {y11}. Alege alt numar!'))
 x21 = float(input('Inrodu valoarea coordonatei x pentru punctul A2!'))
 y21 = float(input('Inrodu valoarea coordonatei y pentru punctul A2!'))
-x22 = float(input('Inrodu valoarea coordonatei x pentru punctul B2!'))
-y22 = float(input('Inrodu valoarea coordonatei y pentru punctul B2!'))
+x22 = float(input('Inrodu valoarea coordonatei x pentru punctul B!'))
+while x22 == x21:
+    x22 = float(input(f'Valoarea coordonatei x pentru punctul B trebuie sa fie diferita de {x21}. Alege alt numar!'))
+y22 = float(input('Inrodu valoarea coordonatei y pentru punctul B!'))
+while y22 == y21:
+    y22 = float(input(f'Valoarea coordonatei x pentru punctul B trebuie sa fie diferita de {y21}. Alege alt numar!'))
 
 p11 = Point(x11, y11)
 p12 = Point(x12, y12)
