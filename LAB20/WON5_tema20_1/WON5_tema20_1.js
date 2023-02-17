39b83e198a34348838bd587882ab1a8cfde17add
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-02-17 21:05:30 +0200

    Tema 19

---
+++ LAB20/WON5_tema20_1/WON5_tema20_1.js
@@ -0,0 +1,13 @@
+function yell_back() {
+    document.getElementById("user_yellow").style.background = 'yellow';
+}
+function no_back() {
+    document.getElementById("user_yellow").style.background = 'white';
+}
+
+function yell_back2() {
+    document.getElementById("pass_yellow").style.background = 'yellow';
+}
+function no_back2() {
+    document.getElementById("pass_yellow").style.background = 'white';
+}
