b10e00e7ad157c341be4829a5f5a7a1976414a30
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-02-21 19:25:10 +0200

    Tema 20

--- LAB20/WON5_tema20_3/WON5_tema20_3.js
+++ LAB20/WON5_tema20_3/WON5_tema20_3.js
@@ -13,12 +13,12 @@ function factorial(n) {
 function check_number () {
     let nb = document.getElementById('num').value;
     let n = parseInt(nb);
-    if (isNaN(n)){
+    if (!isNaN(n) && n>0){
+        let fact = factorial(n);
+        document.getElementById('answer').innerHTML ="<span class='color-green'>Factorialul numarului introdus este:" + fact;
+    }
+    else {
         document.getElementById('answer').innerHTML ="<span class='color-red'>Nu se poate calcula factorialul.";
-        }
-        else{
-            let fact = factorial(n);
-            document.getElementById('answer').innerHTML ="<span class='color-green'>Factorialul numarului introdus este:" + fact;
-        }
+    }
 
 }
