076748f4ca26aa05b4732d08472c57fe44d87ee9
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-02-21 20:23:23 +0200

    Tema 20

--- LAB20/WON5_tema20_4/WON5_tema20_4.js
+++ LAB20/WON5_tema20_4/WON5_tema20_4.js
@@ -1,41 +1,46 @@
 function valid_user() {
     let x = document.forms["sign_in_form"]["name"].value;
+    const elem = document.getElementById('answer_user')
     if (x == "") {
-        document.getElementById('answer_user').innerHTML ="<span class='color-red'>* Username must be filled out";
+        elem.innerHTML =" Username must be filled out";
         return false;
     }
     else {
         if (x.length < 4) {
-             document.getElementById('answer_user').innerHTML ="<span class='color-red'>* Username must be at least 4 characters.";
+             elem.innerHTML =" Username must be at least 4 characters.";
              return false;
         }
         else {
             if  (x.length > 10) {
-                document.getElementById('answer_user').innerHTML ="<span class='color-red'>* Username must be less then 10 characters.";
+                elem.innerHTML =" Username must be less then 10 characters.";
                 return false;
+            } else {
+                elem.innerHTML ="";
             }
         }
     }
+    return true;
 }
 
 function valid_password() {
     let x = document.forms["sign_in_form"]["pass"].value;
     if (x == "") {
-        document.getElementById('answer_pass').innerHTML ="<span class='color-red'>* Password must be filled out";
+        document.getElementById('answer_pass').innerHTML =" Password must be filled out";
         return false;
     }
     else {
         if (x.length < 8) {
-             document.getElementById('answer_pass').innerHTML ="<span class='color-red'>* Password must be at least 8 characters.";
+             document.getElementById('answer_pass').innerHTML =" Password must be at least 8 characters.";
              return false;
         }
         else {
             if  (x.length > 20) {
-                document.getElementById('answer_pass').innerHTML ="<span class='color-red'>* Password must be less then 20 characters.";
+                document.getElementById('answer_pass').innerHTML ="Password must be less then 20 characters.";
                 return false;
             }
         }
     }
+    return true;
 }
 
 function valid_repeat_pass() {
@@ -45,18 +50,31 @@ function valid_repeat_pass() {
         document.getElementById('answer_pass_repeat').innerHTML ="<span class='color-red'>* The password confirmation does not match.";
         return false;
     }
+    return true;
 }
 
 function valid_check() {
     if (document.forms["sign_in_form"]["check"].checked == false) {
         document.getElementById('answer_check').innerHTML ="<span class='color-red'>* Please agree to all the terms and conditions before create an account.";
     }
+    return true;
 }
 
 function validation() {
-    valid_user();
-    valid_password();
-    valid_repeat_pass();
-    valid_check();
+    console.log('ok')
+    if (!valid_user()) {
+        return false;
+    };
+    if (!valid_password()) {
+        return false;
+    };
+    if (!valid_repeat_pass()) {
+        return false;
+    };
+    if (!valid_check()) {
+        return false;
+    };
+    console.log('final')
+    return true;
 }
 
