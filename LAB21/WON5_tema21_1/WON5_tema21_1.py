7fcc0954ee2cceb211f7a3df09fd21c7b697c24f
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-02-23 22:49:36 +0200

    Tema 21

--- LAB21/WON5_tema21_1/WON5_tema21_1.py
+++ LAB21/WON5_tema21_1/WON5_tema21_1.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask('Water-gas Installation')
 
+
+@app.route('/')
 @app.route('/home')
 def home():
-    return render_template()
+    return render_template('home.html')
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
