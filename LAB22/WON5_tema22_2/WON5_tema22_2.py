1324de40d80e1918ae03db5e8815c985fa700a9f
Kaltenbacher Zsolt <kaltenbacher.zsolt@gmail.com>
2023-03-04 20:25:16 +0200

    Tema 22

---
+++ LAB22/WON5_tema22_2/WON5_tema22_2.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask('Students')
+
+
+st = {
+    23: {'fname': 'Dana', 'lname': 'Popescu', 'class': '9b'},
+    2: {'fname': 'Ion', 'lname': 'Pop', 'class': '10c'},
+    31: {'fname': 'Gelu', 'lname': 'Ionescu', 'class': '10c'},
+    15: {'fname': 'Geta', 'lname': 'Ionescu', 'class': '9b'},
+}
+
+
+@app.route('/')
+@app.route('/students/')
+def students():
+    return render_template('students.html', students=st)
+
+
+@app.route('/students/<student_id>/')
+def show_student(student_id):
+    return render_template("student.html", students=st, sid=student_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
